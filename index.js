a6d14018e36d9d8ccdd73d2221c39ce0dcb03a0e
seattleriot <seattleriot@hotmail.com>
2021-11-11 16:59:08 -0800

    added all questions

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ const licenses = require("./utils/licenses.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+// Question to populate README
 const questions = () => {
   return [
     {
       type: "input",
       message: "What is your project title:",
-      name: "projectTitle",
+      name: "title",
     },
     {
       type: "input",
       message: "Please provide a short description of the project:",
-      name: "projectDescription",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Please provide installation instructions:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Please provide usage:",
+      name: "usage",
     },
     {
       type: "list",
@@ -27,17 +38,38 @@ const questions = () => {
         licenses.WTFPL,
       ],
     },
+    {
+      type: "input",
+      message:
+        "Please provide a list of gitHub usernames who contributed (separated by commas):",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Please describe the projects tests:",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Please provide your gitHub username:",
+      name: "gitHubUserName",
+    },
+    {
+      type: "input",
+      message: "Please provide your email address for questions:",
+      name: "email",
+    },
   ];
 };
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
     err ? console.error(err) : console.log("Success!");
   });
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
   inquirer.prompt(questions()).then((answers) => {
     writeToFile("./output/README.md", generateMarkdown(answers));
